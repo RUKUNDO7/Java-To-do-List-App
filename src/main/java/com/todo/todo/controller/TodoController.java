74dcaa2452415a3103aabca44cadbaeb54b2d04b
Rukundo <gihozoRukundobenise@gmail.com>
2026-02-22 16:37:07 +0200

    Created the controller class

---
+++ src/main/java/com/todo/todo/controller/TodoController.java
@@ -0,0 +1,39 @@
+package com.todo.todo.controller;
+
+import com.todo.todo.model.Todo;
+import com.todo.todo.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+@CrossOrigin
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.createTodo(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+        return todoService.updateTodo(id, todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+    }
+}
