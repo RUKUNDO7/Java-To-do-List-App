7426dc8c17f8a58a0283ce914c31a6d50c3ccc62
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-01 01:20:01 +0200

    created the app user model class

---
+++ src/main/java/com/todo/todo/model/AppUser.java
@@ -0,0 +1,31 @@
+package com.todo.todo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "app_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+}
