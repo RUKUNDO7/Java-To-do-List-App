7f1dc6cb365a47c4e1dd91988824e38d569918cd
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-01 01:24:54 +0200

    modified the todo repo file

--- src/main/java/com/todo/todo/repository/TodoRepository.java
+++ src/main/java/com/todo/todo/repository/TodoRepository.java
@@ -2,13 +2,17 @@ package com.todo.todo.repository;
 
 import com.todo.todo.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    Optional<Todo> findByTitle(String title);
-    boolean existsByTitle(String title);
-    void deleteByTitle(String title);
-    java.util.List<Todo> findByStatus(boolean status);
+    List<Todo> findByOwnerId(Long ownerId);
+    List<Todo> findByOwnerIdAndStatus(Long ownerId, boolean status);
+    Optional<Todo> findByOwnerIdAndId(Long ownerId, Long id);
+    Optional<Todo> findByOwnerIdAndTitle(Long ownerId, String title);
+    boolean existsByOwnerIdAndTitle(Long ownerId, String title);
+    void deleteByOwnerIdAndTitle(Long ownerId, String title);
+    long countByOwnerId(Long ownerId);
+    long countByOwnerIdAndStatus(Long ownerId, boolean status);
 }
